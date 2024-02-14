7b983238c7849af0d43d0141fde7494e23c523e0
damorenomed <104732924+damorenomed@users.noreply.github.com>
2024-02-13 22:04:51 -0500

    creacion package 1

---
+++ Garage/src/controlador/Controlador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author Diego Moreno
+ */
+public class Controlador {
+
+    
+    
+}
--- Garage/src/run/Garage.java
+++ Garage/src/run/Garage.java
@@ -1,15 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package garage;
+package run;
 
 /**
  *
  * @author Diego Moreno
  */
 public class Garage {
-
+    
     /**
      * @param args the command line arguments
      */
